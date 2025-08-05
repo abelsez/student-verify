113075e00b4ee39cec4a9102795d8a05c78ed2df
abelsez <abelsez@example.com>
2025-08-05 00:06:19 -0400

    Fix: Use relative paths for assets in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: 
-  process.env.NODE_ENV === 'production'
-    ? '/student-verify/'
-    : '/',
+  base: './',
   build: {
     rollupOptions: {
       input: {
@@ -24,3 +21,5 @@ export default defineConfig({
     },
   },
 })
+
+
